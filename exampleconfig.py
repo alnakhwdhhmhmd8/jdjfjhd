56715cf81eeb40d9255636dcf323b377f331fc49
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-06-10 21:18:35 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -7,9 +7,9 @@ class Development(Config):
     API_HASH = "ضع كود الايبي هاش"
     ALIVE_NAME = "اسم حسابك التلي"
     DB_URI = "رابـط التخـزين الخـاص بك"
-    STRING_SESSION = "كود تيرمــكس"
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
+    STRING_SESSION = "1BJWap1sAUCGgdamVuaI9PCHnHNvJdSSXHd_5bKBXlDWMMeqR-Xsmc8WsazvMsoQZ8lNRBq1kJIftTg_4UMuVMQgMRDPzDNPbJhqTe7sD8QV58vzhUoquyKk9qJLonVSRsFlDoumJG_Bg3Zim68wOrjeIBO5cqtv66J_O1eTyUqakNy9JZcv_WoXXmwTeNvq_umE3vgngI6ZdbVD894AWZwdy2bth1Ec6PhFlO1wugD-G3EHXBPZK3HM428GYnCfUC28vWP98xTfbzpcwa1aM1EglGlkb6aquos7LGUOwJjjocu7xFuEQCgX7iGF6q7XlvxpHVMsAyfkN3FYguZMrODb5iOaRpDM="
+    TG_BOT_TOKEN = "8063434891:AAEJLFvb8XoO-Edt_GVoXjyx7v9YakNcJXA"
+    PRIVATE_GROUP_BOT_API_ID = -1002264057650  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
     COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     TZ = "Asia/Baghdad"  # اتركهــا كمـا هـي
