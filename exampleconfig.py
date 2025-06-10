51d9825e0c4e16e7b6e6dca5e619dd47f3aae891
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-06-10 21:19:46 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -4,8 +4,8 @@ from sample_config import Config
 class Development(Config):
     # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
     APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
-    API_HASH = "ضع كود الايبي هاش"
-    ALIVE_NAME = "اسم حسابك التلي"
+    API_HASH = "63ce9d714e685b721ee2fa5e0ea06a5a"
+    ALIVE_NAME = "𝐆 𝐀 𝐊⁹¹¹"
     DB_URI = "رابـط التخـزين الخـاص بك"
     STRING_SESSION = "1BJWap1sAUCGgdamVuaI9PCHnHNvJdSSXHd_5bKBXlDWMMeqR-Xsmc8WsazvMsoQZ8lNRBq1kJIftTg_4UMuVMQgMRDPzDNPbJhqTe7sD8QV58vzhUoquyKk9qJLonVSRsFlDoumJG_Bg3Zim68wOrjeIBO5cqtv66J_O1eTyUqakNy9JZcv_WoXXmwTeNvq_umE3vgngI6ZdbVD894AWZwdy2bth1Ec6PhFlO1wugD-G3EHXBPZK3HM428GYnCfUC28vWP98xTfbzpcwa1aM1EglGlkb6aquos7LGUOwJjjocu7xFuEQCgX7iGF6q7XlvxpHVMsAyfkN3FYguZMrODb5iOaRpDM="
     TG_BOT_TOKEN = "8063434891:AAEJLFvb8XoO-Edt_GVoXjyx7v9YakNcJXA"
